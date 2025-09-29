1bc8bd84b7b6d285ea8ff7d240a12429c871a8b3
Melpomeni Kallimorou <m.kallimorou@megasoft.gr>
2025-09-29 16:05:31 +0300

    Added edit delete and create buttons

--- EmployeeManagementPortal/Services/EmployeeServices.cs
+++ EmployeeManagementPortal/Services/EmployeeServices.cs
@@ -1,10 +1,17 @@
-﻿namespace EmployeeManagementPortal.Services
-{
-    //public class EmployeeServices: IEmployeeServices
-    //{
-    //    public async Task GetAllEmployees()
-    //    {
-    //        // Implementation to fetch all employees
-    //    }
-    //}
-}
+﻿using System.Security.Cryptography.X509Certificates;
+
+//namespace EmployeeManagementPortal.Services
+//{
+//    public class EmployeeServices : IEmployeeServices
+//    {
+//       private readonly HttpClient _httpClient;
+//         public EmployeeServices(HttpClient httpClient)
+//         {
+//              _httpClient = httpClient;
+//        }
+//        //public async Task<IEnumerable<Employee>> GetAllEmployees()
+//        //{
+//        //    return await JsonConfigurationExtensions.GetFromJsonAsync<IEnumerable<Employee>>(_httpClient, "api/employees");
+//        //}
+//    }
+//}
