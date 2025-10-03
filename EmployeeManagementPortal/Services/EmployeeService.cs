ff41be8e3142c2d7e91c87507cb9586a55dc1aa6
Melpomeni Kallimorou <m.kallimorou@megasoft.gr>
2025-10-03 17:07:12 +0300

    final changes in client

--- EmployeeManagementPortal/Services/EmployeeService.cs
+++ EmployeeManagementPortal/Services/EmployeeService.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Shared.Models;
 using System.Net.Http.Json;
 
+
 namespace EmployeeManagement.Portal.Services
 {
     public class EmployeeService
@@ -12,30 +13,30 @@ namespace EmployeeManagement.Portal.Services
                 _httpClient = httpClient;
             }
 
-        public async Task<List<Employee>> GetAllEmployeesAsync()
+        public async Task<List<Employee>> GetAll()
         {
-
             return await _httpClient.GetFromJsonAsync<List<Employee>>("api/Employee/GetAll");
         }
 
-        public async Task<Employee?> GetEmployeeById(int id)
+        public async Task<Employee?> GetById(int id)
         {
             return await _httpClient.GetFromJsonAsync<Employee>($"api/Employee/{id}");
         }
 
-        public async Task<bool> AddEmployeeAsync(Employee newEmployee)
+        public async Task<bool> Add(Employee newEmployee)
         {
             var response = await _httpClient.PostAsJsonAsync("api/Employee/Add/", newEmployee);
             return response.IsSuccessStatusCode;
         }
-        public async Task<bool> UpdateEmployeeAsync(Employee updatedEmployee)
+        public async Task<bool> UpdateEmployee(Employee updatedEmployee)
         {
-            var response = await _httpClient.PutAsJsonAsync($"/api/Employee/Update{updatedEmployee.Id}", updatedEmployee);
+            var response = await _httpClient.PutAsJsonAsync($"api/Employee/Update", updatedEmployee);
+
             return response.IsSuccessStatusCode;
         }       
-        public async Task<bool> DeleteEmployeeAsync(int id)
+        public async Task<bool> DeleteById(int id)
         {
-            var response = await _httpClient.DeleteAsync($"api/Employee/DeleteById/{id}");
+            var response = await _httpClient.DeleteAsync($"api/Employee/DeleteById?id={id}");
             return response.IsSuccessStatusCode;
         }
     }
